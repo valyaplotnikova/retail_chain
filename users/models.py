ae53aae6d345030c8e8e2c53b00022b26f987774
valya_plotnikova <valya_ezhova@mail.ru>
2024-12-10 15:07:44 +0300

    init

---
+++ users/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='email')
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
+
+
